6fa395ea6447b25e73e665ca3eb3c607554d508e
AndrewSmarty <mav1996@mail.ru>
2015-01-27 18:00:03 -0800

    Baby Track 1.0.1
    bugs correction, im trying to make out some powerful algorithms for
    block aavatar structure, and combining images with radiobuttons in their
    synchronous work. when it's done it's easier to overcome  other
    creation steps and get rid of plenty of bugs and errors

--- js/background.js
+++ js/background.js
@@ -8,8 +8,23 @@ var BabyTrackMode = {
 	NONE: 0,
 	CREATE_NEW_PERSON: 1,
 	ADD_EVENT_WIZARD_NEW_PERSON: 2,
-	ADD_EVENT_WIZARD_NEW_ACTIVITY: 3
+	ADD_EVENT_WIZARD_NEW_ACTIVITY: 3,
+	CHOOSE_EXISTED_PERSON: 4
+}
+
+var BabyTrackWindows = {
+	NONE: 0,
+	WELCOME_POST: 1,
+	POSTED_RESULTS_TABLE: 2,
+	CREATE_NEW_PERSON: 3,
+	ADD_EVENT_WIZARD_NEW_PERSON: 4,
+	ADD_EVENT_WIZARD_NEW_ACTIVITY: 5,
+	CHOOSE_EXISTED_PERSON: 6
 }
 
 var mode = BabyTrackMode.NONE;
 var initialPage = BabyTrackInitialPage.WELCOME_POST;
+var previousWindow = BabyTrackWindows.NONE;
+//var currentWindow; // DOM object
+
+var windowsAnimationOver = false;
